6c08acc24299ea1d1175cca96a9405ea6e37c549
crotten <wildrunts@gmail.com>
2017-02-20 16:34:15 -0500

    Added in rebase to help manage and retreive my data.  I also am working on the form where users will be able to input reviews.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import * as firebase from 'firebase';
-
-const config = {
-  apiKey: "AIzaSyBYtoV4M6Mt7FQvpSNDTboYCh_gnIcXHhc",
-  authDomain: "dogsite-42aea.firebaseapp.com",
-  databaseURL: "https://dogsite-42aea.firebaseio.com",
-  storageBucket: "dogsite-42aea.appspot.com",
-  messagingSenderId: "299867827949"
-};
-
-firebase.initializeApp(config);
   
 class App extends React.Component {
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import Rebase from 're-base';
+import CreatePosts from './CreatePosts';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBYtoV4M6Mt7FQvpSNDTboYCh_gnIcXHhc",
+  authDomain: "dogsite-42aea.firebaseapp.com",
+  databaseURL: "https://dogsite-42aea.firebaseio.com",
+  storageBucket: "dogsite-42aea.appspot.com",
+  messagingSenderId: "299867827949"
+});
 
 class Product extends Component {
 
+	constructor(props) {
+        super(props);
+        this.state = {
+            products: [],
+            loading: true
+        }
+    }
+
+    componentDidMount() {
+        this.ref = base.syncState('reviews/' + this.props.params.productName, {
+            context: this,
+            state: 'products',
+            asArray: true,
+            then(){
+                this.setState({loading: false})
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        base.removeBinding(this.ref);
+    }
+
   	render() {
-  		console.log(this.props.route.path);
+  		const origTitle = this.props.params.productName;
+  		const postTitle = origTitle.replace(/_/g, ' ');
     	return (
 	      	<div className="">
-	      		<h1>THIS IS THE PRODUCT PAGE</h1>
+	      		<h1>The Top {postTitle}</h1>
+	      		{this.state.loading === true ? <h3> LOADING... </h3> : <CreatePosts posts={this.state.products} />}
+
 	      	</div>
     	);
   	}
