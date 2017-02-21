62527269b419e87ba86cd119adb92bfa86e84066
crotten <wildrunts@gmail.com>
2017-02-20 23:21:19 -0500

    Added the ability to submit a new product review to the DB from the site.

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -16,12 +16,15 @@ class UserForm extends Component {
 	constructor(props) {
 	    super(props);
 	    this.state = {
-	    	numProducts: 1
+	    	numProducts: 1,
+	    	productData: [],
+	    	submitted: false
 	    };
 
 	    this.handleInputChange = this.handleInputChange.bind(this);
 	    this.addProduct = this.addProduct.bind(this);
 	    this.deleteProduct = this.deleteProduct.bind(this);
+	    this.addProductToData = this.addProductToData.bind(this);
 	}
 
 	handleInputChange(event) {
@@ -34,9 +37,34 @@ class UserForm extends Component {
 	    });
 	}
 
-	addProductToData() {
-		base.post('test/chris', {
-			data: {name: 'Chris Otten', age: 25},
+	handleData(data) {
+		let formData = [];
+		
+		for (let i = 0; i < data.numProducts; i++) {
+
+			const productName = 'productName_' + i;
+			const productLink = 'productLink_' + i;
+			const productPrice = 'productPrice_' + i;
+			const productDesc = 'productDescription_' + i;
+
+			formData.push({
+				title: data[productName],
+				link: data[productLink],
+				price: data[productPrice],
+				desc: data[productDesc]
+			});
+		}
+
+		return formData;
+	}
+
+	addProductToData(e) {
+		e.preventDefault();
+		const category = this.state.productCategory.replace(/\s+/g, '_').toLowerCase();
+		const newProducts = this.handleData(this.state);
+
+		base.post('userReviews/' + category, {
+			data: newProducts,
 			then(err) {
 				if(!err) {
 					alert('This was a success!');
@@ -79,10 +107,10 @@ class UserForm extends Component {
 
 	render() {
 		const products = this.displayProductFormFields();
-		console.log(this.state);
     	return (
     		<div className="">
-        		<form>
+    			{this.state.submitted === false ? 
+        		<form onSubmit={this.addProductToData}>
         			<label>
 		        		Product Category:
 		        		<input type="text" name="productCategory" onChange={this.handleInputChange} required></input>
@@ -91,6 +119,7 @@ class UserForm extends Component {
 		        	<button onClick={this.addProduct}>Add another product</button>
 		        	<input type="submit" value="Submit"></input>
 		        </form>
+		    	: <h2>Thank you for submitting a product for review</h2>}
       		</div>
     	);
   	}
